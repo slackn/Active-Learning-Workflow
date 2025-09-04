81adb28072ae4b0ae43d82c6a9e6d74fe4497efd
slackn <silahorozoglu@gmail.com>
2025-09-04 16:17:03 +0200

    Added the slurm script

---
+++ relabel/relabel_job.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --time=10:00:00      # adjust as needed
+#SBATCH --partition=cpu
+#SBATCH --nodes=1
+#SBATCH --ntasks=1          # only one task
+#SBATCH --cpus-per-task=1    # only one thread
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=sila.horozoglu@sabanciuniv.edu
+#SBATCH --job-name=al_Na8
+
+eval "$(conda shell.bash hook)"
+conda activate gpaw-env
+python relabel.py
